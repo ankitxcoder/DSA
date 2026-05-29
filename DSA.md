eeeabf710a89452b1ae884a70352e629a0f61d84
AnkitX <ankitone02@gmail.com>
2026-05-29 23:56:37 +0530

    1st Day array reversal

---
+++ DSA.md
@@ -0,0 +1,15 @@
+# Time Complexity
+big-O -bad
+Theta -average
+Omega -best
+
+## Input size ( CPU Operation )
+### O(1) < O( log n ) < O(n) < O(n^2) < O(n^3) < O(2^x) < O(n!)
+
+## Time Complexity Directky proportional to Space complexity 
+## Time Com. Inversily Proportional to space then --> priority Chosen to Time complexity for doing coding
+
+# Recurrence Relations
+1) Substitution method 
+2) recursion Method 
+3) Master Theorem
---
+++ ReversalArray.java
@@ -0,0 +1,17 @@
+public class ReversalArray {
+    public static void main(String[] args) {
+        int array[] = { 1, 2, 3, 4, 5, 6 };
+        int n = array.length;
+        for (int i = 0; i < n / 2; i++) {
+            int temp = array[i];
+            array[i] = array[n - i - 1];
+            array[n - i - 1] = temp;
+        }
+
+        System.out.println("Reversal of array");
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i]+" ");
+        }
+       
+    }
+}
